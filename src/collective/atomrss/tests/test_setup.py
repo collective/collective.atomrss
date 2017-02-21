371905ea749b7f74830d7bd0dcee26e38643cd4f
Benoît Suttor <bsuttor@imio.be>
2017-02-21 10:43:16 +0100

    Remove unittest2

--- src/collective/atomrss/tests/test_setup.py
+++ src/collective/atomrss/tests/test_setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import unittest2 as unittest
+import unittest
 from plone.browserlayer import utils
 from zope.event import notify
 from zope.traversing.interfaces import BeforeTraverseEvent
